005308dc435d6a7d3578dc0d94120fe073e10099
SNIPPETBUCKET TECH, TEAM <tejas.tank.mca@gmail.com>
2023-07-26 23:53:29 +0530

    Correction applied, now improved code applied. - tejas tank

--- app.js
+++ app.js
@@ -1,68 +1,73 @@
 var Workspace = Backbone.Router.extend({
 
   routes: {
+     "": "help",
     "help":                 "help",    // #help
     "search/:query":        "search",  // #search/kiwis
     "search/:query/p:page": "search"   // #search/kiwis/p7
   },
 
   help: function() {
-    alert();
+       console.log("helping is my help route");
   },
 
   search: function(query, page) {
-        alert();
+        console.log("nice to hearing you.");
   }
 
 });
+var t =  new Workspace();
+Backbone.history.start();
+// FOR ROUTER ITS IS ESSENTIAL TO HAVE Backbone.history.start(); after initialize (like var t =  new Workspace();) then only it works well.
 
 
-   var engine = new QWeb2.Engine("./src/templates/templates.xml")
-        engine.debug = true;
-        
-        var def = $.Deferred();
-        engine.add_template('./src/templates/templates.xml', function(err) {
-            if (err) {
-                def.reject(err);
-            } else {
-                def.resolve();
-                
-            }
-        });
-        
-        
-  var Widget = Backbone.View.extend({
-        initialize: function(option){
-           console.log("widget option data: ", option || {});
-	       this.render(); // render template			
-        },
-        render: function(){
-           this.$el.append("<h1>working</h1>");
-           console.log("this.el",this.el);
-           console.log("this.$el",this.$el);
-        },
-        events: {
-	      "click h1": "warn",
-        },
-        warn: function(){
-           throw Error("Great Error Message.");
-        },
-        destroy: function(){
-          this.el.remove();
-          this.unbind();
-        }
-    });
-    
-   $(function() {
-       var input = new Widget();
-       input.$el.appendTo($("#box1"));
-       
-       console.log(engine.render('xbox'));
-       
-   });
-   
-   window.onerror = function(message, url, linenumber) {
-        console.log("message: ", message);
-      alert("JavaScript error: " + message + " on line " + linenumber + " for " + url);
-   }
+var Widget = Backbone.View.extend({
+    initialize: function(option){
+       console.log("widget option data: ", option || {});
+       this.render(); // render template
+    },
+    render: function(){
+       this.$el.append("<h1>working</h1>");
+       console.log("this.el",this.el);
+       console.log("this.$el",this.$el);
+    },
+    events: {
+      "click h1": "warn",
+    },
+    warn: function(){
+       throw Error("Great Error Message.");
+    },
+    destroy: function(){
+      this.el.remove();
+      this.unbind();
+    }
+});
+
+// NOW LOADING TEMPLATE ENGINE SYSTEM
+var engine = new QWeb2.Engine("./src/templates/templates.xml")
+engine.debug = false;
+
+var def = $.Deferred();
+// Template will be loaded in async so use deffered , once load successful then def.resolve(); as works. execute code ahead.
+// for any bocking ask me tejas@xamta.in
+engine.add_template('./src/templates/templates.xml', function(err) {
+    if (err) {
+        def.reject(err);
+    } else {
+        def.resolve();
+         $(function() {
+           var input = new Widget();
+           input.$el.appendTo($("#box1"));
+          
+           
+            console.log(engine.render('xbox'));
+       });
+    }
+});
+
+window.onerror = function(message, url, linenumber) {
+    console.log("message: ", message);
+  alert("JavaScript error: " + message + " on line " + linenumber + " for " + url);
+}
+
 
